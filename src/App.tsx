cd1c493c5e453177800a133808fe885a3430b17b
Robani Diansyah <melinting522@gmail.com>
2025-09-06 21:40:22 +0700

    feat: menambahkan komponen Hero ke App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import "./index.css";
 import Navbar from './components/Navbar';
+import Hero from "./components/Hero";
+import "./index.css";
 
 
 function App() {
-  
-
   return (
-    <Navbar />
-  )
+    <div className="dark">
+      <Navbar />
+      <Hero />
+    </div>
+  );
 }
 
-export default App
+export default App;
