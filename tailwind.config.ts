a41ec0c9b424aa1f92778c7dc91ba9ecd8037861
Robani Diansyah <melinting522@gmail.com>
2025-09-06 21:14:43 +0700

    initial commit: setup project portfolio

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,22 @@
-import type {Config} from "tailwindcss";
+import type {Config}
+from "tailwindcss";
 
 const config: Config = {
+    darkMode: "class", // ✅ wajib biar dark mode jalan
     content: [
         "./index.html", "./src/**/*.{js,ts,jsx,tsx}"
     ],
     theme: {
-        extend: {}
-    }
+        extend: {
+            colors: {
+                futuristic: {
+                    500: "#00FFFF", // neon cyan
+                    600: "#00E5E5"
+                }
+            }
+        }
+    },
+    plugins: []
 };
 
 export default config;
