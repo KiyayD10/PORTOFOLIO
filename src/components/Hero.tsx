328f43c17c86b21e323edbffd155d2a603e940d7
Robani Diansyah <melinting522@gmail.com>
2025-09-06 21:39:33 +0700

    style: melakukan styling pada komponen Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,14 +3,16 @@ import { motion } from "framer-motion";
 
 const Hero = () => {
     return(
-        <section id="home">
+        <section id="home"
+        className="h-screen flex items-center justify-center text-center bg-gradient-to-b from-black to-gray-900 dark:from-white dark:to-gray-100"
+        >
             <motion.div
             initial={{ opacity: 0, y: 40}}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1 }}
             >
-                <h1>
-                    Hi, I&apos;m <span>Robani Diansyah</span>
+                <h1 className="text-4xl md:text-6xl font-bold mb-4">
+                    Hi, I&apos;m <span className="text-futuristic-500">Robani Diansyah</span>
                 </h1>
                 <TypeAnimation
                 sequence={[
@@ -20,7 +22,7 @@ const Hero = () => {
                 wrapper="span"
                 speed={50}
                 repeat={Infinity}
-                />
+                className="text-2xl md:text-3xl text-futuristic-500"/>
             </motion.div>
         </section>
     );
